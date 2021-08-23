68ce3e0c5c281104b1655dab5e7d343c924e48c4
Osrhu <h1147246925@163.com>
2021-08-23 09:42:59 +0800

    support non-zero time

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from job import daka
 
 def main():
     config = Configuration.getConfig()
-    schedule.every().day.at(config["schedule"]).do(daka)
+    total = int(config["schedule"])
+    try:
+        schedule.every().day.at(config["schedule"]).do(daka)
+    except Exception as e:
+        hh = total / 60
+        mm = total % 60
+        pattern = r'%02d:%02d'
+        config["schedule"] = pattern % (hh, mm)
+        schedule.every().day.at(config["schedule"]).do(daka)
     while True:
         schedule.run_pending()
 
